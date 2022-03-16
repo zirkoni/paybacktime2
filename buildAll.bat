441acacaf1291d50c5c2c93ce566d2f8b0398e35
zirkoni <zirkoni88@hotmail.com>
2022-03-16 17:36:19 +0200

    Investigate runtime problems
    - Game1.dat fails to load if *.asm files are compiled with "not enough
      conventional memory" error
    - Also text is not visible in menus with re-compiled asm-files
    - With original *.obj files game1.dat freezes when loading
    
    - Remove INTER.ASM, the code is actually included in ASMTOOLS.ASM

--- buildAll.bat
+++ buildAll.bat
@@ -1,11 +1,10 @@
 cd src
 
-tasm ASMTOOLS.ASM
-tasm BLASTER.ASM
-tasm BUMP_ASM.ASM
-::tasm INTER.ASM Does not compile, lots of errors: Code or data emissions to undeclared segment
-tasm RADPLAY.ASM
-tasm ROUTE.ASM
+::tasm ASMTOOLS.ASM
+::tasm BLASTER.ASM
+::tasm BUMP_ASM.ASM
+::tasm RADPLAY.ASM
+::tasm ROUTE.ASM
 
 call tp.bat
 call regc.bat
