94aa1074e471e99383f23d02d204a43436383b14
zirkoni <zirkoni88@hotmail.com>
2022-03-17 16:06:16 +0200

    Fix asm warnings
    Fixes menu and crash issues.
    Game still freezes when loading the player selection.

--- buildAll.bat
+++ buildAll.bat
@@ -1,10 +1,10 @@
 cd src
 
-::tasm ASMTOOLS.ASM
-::tasm BLASTER.ASM
-::tasm BUMP_ASM.ASM
-::tasm RADPLAY.ASM
-::tasm ROUTE.ASM
+tasm ASMTOOLS.ASM
+tasm BLASTER.ASM
+tasm BUMP_ASM.ASM
+tasm RADPLAY.ASM
+tasm ROUTE.ASM
 
 call tp.bat
 call regc.bat
